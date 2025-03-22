7fd2fd50c136e887faf481e251e91ad933e517d6
Ana Julia <117485526+ajlima12@users.noreply.github.com>
2025-03-22 11:20:11 -0300

    Comentado com as devidas explicações.

--- src/main/java/buttons/WindowsButton.java
+++ src/main/java/buttons/WindowsButton.java
@@ -8,10 +8,13 @@ package buttons;
  *
  * @author FATEC ZONA LESTE
  */
+// A classe WindowsButton implementa a interface Button e define como o botão será desenhado no Windows.
 public class WindowsButton implements Button {
 
+    // Implementação do método 'paint' que imprime uma mensagem no console.
     @Override
     public void paint() {
         System.out.println("You have created WindowsButton.");
     }
 }
+
