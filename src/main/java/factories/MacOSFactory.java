1e12c85199946745518b8421c0fa1dd533862ad2
Ana Julia <117485526+ajlima12@users.noreply.github.com>
2025-03-22 10:57:30 -0300

    Comentado com as devidas explicações.

--- src/main/java/factories/MacOSFactory.java
+++ src/main/java/factories/MacOSFactory.java
@@ -13,15 +13,22 @@ import checkboxes.MacOSCheckbox;
  *
  * @author FATEC ZONA LESTE
  */
+// A classe MacOSFactory implementa a interface GUIFactory.
+// MacOSFactory é responsável por criar os componentes gráficos específicos para o sistema operacional macOS.
 public class MacOSFactory implements GUIFactory {
 
+    // Este método cria um botão específico para o macOS.
+    // Quando chamado, ele retorna um novo objeto da classe MacOSButton, que é uma implementação do botão para o macOS.
     @Override
     public Button createButton() {
-        return new MacOSButton();
+        return new MacOSButton();  // Retorna um botão macOS.
     }
 
+    // Este método cria uma caixa de seleção específica para o macOS.
+    // Quando chamado, ele retorna um novo objeto da classe MacOSCheckbox, que é uma implementação da caixa de seleção para o macOS.
     @Override
     public Checkbox createCheckbox() {
-        return new MacOSCheckbox();
+        return new MacOSCheckbox();  // Retorna uma caixa de seleção macOS.
     }
 }
+
