9a0c18bf25ca258cb9d60360306b637dd68cd00e
Ana Julia <117485526+ajlima12@users.noreply.github.com>
2025-03-22 11:21:58 -0300

    Comentado com as devidas explicações.

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -12,17 +12,20 @@ import factories.GUIFactory;
  *
  * @author FATEC ZONA LESTE
  */
+// A classe Application usa os objetos Button e Checkbox para criar e desenhar uma interface gráfica.
 public class Application {
-    private Button button;
-    private Checkbox checkbox;
+    private Button button;    // Declaração de um botão.
+    private Checkbox checkbox; // Declaração de uma caixa de seleção.
 
+    // O construtor recebe (GUIFactory) e usa ela para criar o botão e a caixa de seleção.
     public Application(GUIFactory factory) {
-        button = factory.createButton();
-        checkbox = factory.createCheckbox();
+        button = factory.createButton();  // Cria o botão usando a Factory.
+        checkbox = factory.createCheckbox(); // Cria a caixa de seleção usando a Factory.
     }
 
+    // Método para desenhar o botão e a caixa de seleção.
     public void paint() {
-        button.paint();
-        checkbox.paint();
+        button.paint();    // Desenha o botão.
+        checkbox.paint();  // Desenha a caixa de seleção.
     }
 }
