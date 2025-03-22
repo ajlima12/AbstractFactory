8a14350e125ec4b662c1119a23e8dfba0d46ef74
Ana Julia <117485526+ajlima12@users.noreply.github.com>
2025-03-22 11:12:59 -0300

    Update WindowsCheckbox.java

--- src/main/java/checkboxes/WindowsCheckbox.java
+++ src/main/java/checkboxes/WindowsCheckbox.java
@@ -8,10 +8,13 @@ package checkboxes;
  *
  * @author FATEC ZONA LESTE
  */
-public class WindowsCheckbox implements Checkbox {
+// Implementação da caixa de seleção específica para Windows.  
+public class WindowsCheckbox implements Checkbox {  
 
-    @Override
-    public void paint() {
-        System.out.println("You have created WindowsCheckbox.");
-    }
-}
+    // Sobrescreve o método paint() para exibir a caixa de seleção no estilo Windows.  
+    @Override  
+    public void paint() {  
+        System.out.println("You have created WindowsCheckbox."); // Exibe uma mensagem indicando a criação da caixa de seleção.  
+    }  
+}  
+
