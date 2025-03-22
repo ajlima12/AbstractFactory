12a999c038e65ab9063686f62f03a7d289f485d3
ajlima12 <ajlimaa12@gmail.com>
2025-03-21 22:19:57 -0300

    nadjulai

---
+++ src/main/java/Demo.java
@@ -0,0 +1,35 @@
+
+import app.Application;
+import factories.GUIFactory;
+import factories.MacOSFactory;
+import factories.WindowsFactory;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class Demo {
+
+    private static Application configureApplication() {
+        Application app;
+        GUIFactory factory;
+        String osName = System.getProperty("os.name").toLowerCase();
+        if (osName.contains("mac")) {
+            factory = new MacOSFactory();
+        } else {
+            factory = new WindowsFactory();
+        }
+        app = new Application(factory);
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Application app = configureApplication();
+        app.paint();
+    }
+}
---
+++ src/main/java/factories/MacOSFactory.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factories;
+
+import buttons.Button;
+import buttons.MacOSButton;
+import checkboxes.Checkbox;
+import checkboxes.MacOSCheckbox;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class MacOSFactory implements GUIFactory {
+
+    @Override
+    public Button createButton() {
+        return new MacOSButton();
+    }
+
+    @Override
+    public Checkbox createCheckbox() {
+        return new MacOSCheckbox();
+    }
+}
