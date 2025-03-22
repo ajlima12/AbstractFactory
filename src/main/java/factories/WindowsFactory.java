34979aae5655abed109bdd3a31affc17c5c74184
Ana Julia <117485526+ajlima12@users.noreply.github.com>
2025-03-22 11:07:33 -0300

    Comentado com as devidas explicações.

--- src/main/java/factories/WindowsFactory.java
+++ src/main/java/factories/WindowsFactory.java
@@ -13,15 +13,22 @@ import checkboxes.WindowsCheckbox;
  *
  * @author FATEC ZONA LESTE
  */
+// A classe WindowsFactory implementa a interface GUIFactory.
+// WindowsFactory é responsável por criar os componentes gráficos específicos para o sistema operacional Windows.
 public class WindowsFactory implements GUIFactory {
 
+    // Este método cria um botão específico para o Windows.
+    // Quando chamado, ele retorna um novo objeto da classe WindowsButton, que é uma implementação do botão para o Windows.
     @Override
     public Button createButton() {
-        return new WindowsButton();
+        return new WindowsButton(); // Retorna um botão Windows.
     }
 
+    // Este método cria uma caixa de seleção específica para o Windows.
+    // Quando chamado, ele retorna um novo objeto da classe WindowsCheckbox, que é uma implementação da caixa de seleção para o Windows.
     @Override
     public Checkbox createCheckbox() {
-        return new WindowsCheckbox();
+        return new WindowsCheckbox(); // Retorna uma caixa de seleção Windows.
     }
 }
+
